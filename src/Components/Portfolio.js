c42c2a5bca6688585dcee74e79478b1f1d9ee55f
Samarth Roy <samarthroy2106@gmail.com>
2022-06-17 18:59:33 +0530

    First commit

---
+++ src/Components/Portfolio.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class Portfolio extends Component {
+  render() {
+
+    if(this.props.data){
+      var projects = this.props.data.projects.map(function(projects){
+        var projectImage = 'https://samroy2106.github.io/ba-portfolio/images/portfolio/'+projects.image;
+        return <div key={projects.title} className="columns portfolio-item">
+           <div className="item-wrap">
+            <a href={projects.url} title={projects.title} target="_blank" rel="noopener noreferrer">
+               <img alt={projects.title} src={projectImage}/>
+               <div className="overlay">
+                  <div className="portfolio-item-meta">
+                 <h5>{projects.title}</h5>
+                     <p>{projects.category}</p>
+                  </div>
+                </div>
+            </a>
+          </div>
+        </div>
+      })
+    }
+
+    return (
+      <section id="portfolio">
+
+      <div className="row">
+
+         <div className="twelve columns collapsed">
+
+            <h1>Check Out Some of My Projects.</h1>
+
+            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+                {projects}
+            </div>
+          </div>
+      </div>
+   </section>
+    );
+  }
+}
+
+export default Portfolio;
